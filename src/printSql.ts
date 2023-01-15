3519bf7f12e4a42b01216dfe8829da37e939666e
Rene Saarsoo <nene@triin.net>
2023-01-15 22:21:52 +0200

    Move printNode and printSql into single file

--- src/printSql.ts
+++ src/printSql.ts
@@ -1,8 +1,9 @@
 import { Node } from "sql-parser-cst";
 import { AstPath, Doc, ParserOptions } from "prettier";
-import { printNode } from "./printNode";
 import { PrintFn } from "./PrintFn";
-import { isArray, isDefined } from "./utils";
+import { isArray, isDefined, isString } from "./utils";
+import { transformMap } from "./syntax";
+import { NodeByType, ToDocFn } from "./CstToDocMap";
 
 export function printSql(
   path: AstPath<Node>,
@@ -19,4 +20,32 @@ export function printSql(
   };
 
   return printNode(path, options, print);
+}
+
+function printNode(
+  path: AstPath<Node>,
+  options: ParserOptions<Node>,
+  print: PrintFn<Node>
+): Doc {
+  const node = path.getValue();
+
+  if (isArray(node)) {
+    return path.map(print);
+  }
+  if (isString(node)) {
+    return node;
+  }
+
+  const fn = transformMap[node.type] as ToDocFn<
+    NodeByType<(typeof node)["type"], Node>
+  >;
+
+  if (!fn) {
+    if (!node.type) {
+      throw new Error(`No type field on node: ${JSON.stringify(node)}`);
+    }
+    throw new Error(`Unexpected node type: ${node.type}`);
+  }
+
+  return fn(print, path, options);
 }
