51fc10bfa59e9ff8760f8e6bb6e94c44c9adc0d2
Rene Saarsoo <nene@triin.net>
2023-01-16 12:58:04 +0200

    Move expression tests to separate file

--- test/select.test.ts
+++ test/select.test.ts
@@ -135,36 +135,6 @@ describe("select", () => {
     `);
   });
 
-  it(`formats binary expressions`, () => {
-    expect(
-      pretty(`SELECT 1 + 2 / 3 * (5 - 1), true OR false AND true`, {
-        printWidth: 20,
-      })
-    ).toBe(dedent`
-      SELECT
-        1 + 2 / 3 * (5 - 1),
-        true OR false AND true
-    `);
-  });
-
-  it(`formats function call to single line`, () => {
-    expect(pretty(`SELECT sqrt(1, 2, 3)`, { printWidth: 15 })).toBe(dedent`
-      SELECT
-        sqrt(1, 2, 3)
-    `);
-  });
-
-  it(`formats function call to multiple lines`, () => {
-    expect(pretty(`SELECT sqrt(1, 2, 3)`, { printWidth: 10 })).toBe(dedent`
-      SELECT
-        sqrt(
-          1,
-          2,
-          3
-        )
-    `);
-  });
-
   it(`formats statement ending with semicolon`, () => {
     expect(pretty(`SELECT 1;`)).toBe(dedent`
       SELECT 1;
