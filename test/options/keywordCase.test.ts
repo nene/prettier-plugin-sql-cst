5ae3dcc4425b34bca48952d809d8a8fa858332be
Rene Saarsoo <nene@triin.net>
2023-01-20 17:40:45 +0200

    Add missing keywordCase test

---
+++ test/options/keywordCase.test.ts
@@ -0,0 +1,34 @@
+import dedent from "dedent-js";
+import { pretty } from "../test_utils";
+
+describe("sqlKeywordCase option", () => {
+  it(`sqlKeywordCase: "preserve" keeps keywords case as-is`, () => {
+    expect(
+      pretty(`select * From tbl WHERE x > 0`, {
+        sqlKeywordCase: "preserve",
+      })
+    ).toBe(dedent`
+      select * From tbl WHERE x > 0
+    `);
+  });
+
+  it(`sqlKeywordCase: "upper" converts keywords to uppercase`, () => {
+    expect(
+      pretty(`select * From tbl WHERE x > 0`, {
+        sqlKeywordCase: "upper",
+      })
+    ).toBe(dedent`
+      SELECT * FROM tbl WHERE x > 0
+    `);
+  });
+
+  it(`sqlKeywordCase: "lower" converts keywords to lowercase`, () => {
+    expect(
+      pretty(`select * From tbl WHERE x > 0`, {
+        sqlKeywordCase: "lower",
+      })
+    ).toBe(dedent`
+      select * from tbl where x > 0
+    `);
+  });
+});
