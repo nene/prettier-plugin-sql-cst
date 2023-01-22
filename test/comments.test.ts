6f5ec63735dcffb93d7cc71a8a5538f4b3bcdc7d
Rene Saarsoo <nene@triin.net>
2023-01-22 18:14:06 +0200

    Additional comment tests

--- test/comments.test.ts
+++ test/comments.test.ts
@@ -6,6 +6,25 @@ describe("comments", () => {
     test(dedent`
       /* leading comment */
       SELECT 1, /*com1*/ 2 /*com2*/
+      /* trailing comment */
+    `);
+  });
+
+  it(`formats basic doc-comments`, () => {
+    test(dedent`
+      /**
+       * A large doc-comment comment
+       * inside this block of code
+       */
+      SELECT 1, 2, 3
+    `);
+  });
+
+  it(`formats block comments between syntax elements`, () => {
+    test(dedent`
+      CREATE /*c1*/ TABLE /*c2*/ IF /*c3*/ NOT EXISTS /*c4*/ foo (
+        id /*c5*/ INT /*c6*/ NOT /*c7*/ NULL
+      )
     `);
   });
 
@@ -14,6 +33,7 @@ describe("comments", () => {
       -- first line comment
       -- second line comment
       SELECT 1 -- third line comment
+      -- final comment
     `);
   });
 
