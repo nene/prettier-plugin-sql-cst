74cde476b322c7aa111b8fd8ad599428966a5a82
Rene Saarsoo <nene@triin.net>
2023-01-22 18:05:20 +0200

    Separate out comment movement test

--- test/comments.test.ts
+++ test/comments.test.ts
@@ -3,14 +3,7 @@ import { pretty, test } from "./test_utils";
 
 describe("comments", () => {
   it(`formats block comments`, () => {
-    expect(
-      pretty(
-        `/* leading comment */
-        SELECT
-          1 /*com1*/,
-          2 /*com2*/`
-      )
-    ).toBe(dedent`
+    test(dedent`
       /* leading comment */
       SELECT
         1, /*com1*/
@@ -23,6 +16,20 @@ describe("comments", () => {
       -- first line comment
       -- second line comment
       SELECT 1 -- third line comment
+    `);
+  });
+
+  it(`moves block comments before comma to line ends`, () => {
+    expect(
+      pretty(`
+        SELECT
+          1 /*com1*/,
+          2
+      `)
+    ).toBe(dedent`
+      SELECT
+        1, /*com1*/
+        2
     `);
   });
 });
