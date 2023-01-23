e9c8453d031e95cf8c30990ad7f7d68cc6019cac
Rene Saarsoo <nene@triin.net>
2023-01-23 12:42:39 +0200

    Refactor select from tests to use the test() helper

--- test/select_from.test.ts
+++ test/select_from.test.ts
@@ -1,34 +1,22 @@
 import dedent from "dedent-js";
-import { pretty, test } from "./test_utils";
+import { test } from "./test_utils";
 
 describe("select FROM", () => {
   it(`formats select with short join to single line`, () => {
     test(`SELECT * FROM client NATURAL JOIN client_sale`);
   });
 
-  it(`formats FROM with a join to multiple lines`, () => {
-    expect(
-      pretty(
-        `SELECT * FROM client LEFT JOIN client_sale ON client_sale.client_id = client.id`,
-        { printWidth: 60 }
-      )
-    ).toBe(dedent`
+  it(`formats FROM with a long join to multiple lines`, () => {
+    test(dedent`
       SELECT *
       FROM
-        client
-        LEFT JOIN client_sale ON client_sale.client_id = client.id
+        client_relation
+        LEFT JOIN client_sale ON client_sale.client_id = client_relation.id
     `);
   });
 
   it(`formats FROM with multiple joins to multiple lines`, () => {
-    expect(
-      pretty(
-        `SELECT * FROM client
-        LEFT JOIN client_sale ON client_sale.client_id = client.id
-        RIGHT OUTER JOIN client_attribute ON client_attribute.client_id = client.id`,
-        { printWidth: 80 }
-      )
-    ).toBe(dedent`
+    test(dedent`
       SELECT *
       FROM
         client
@@ -38,14 +26,7 @@ describe("select FROM", () => {
   });
 
   it(`formats FROM joins with USING-specification`, () => {
-    expect(
-      pretty(
-        `SELECT * FROM client
-        LEFT JOIN client_sale USING (client_id)
-        RIGHT OUTER JOIN client_attribute USING (client_attrib_id, client_id)`,
-        { printWidth: 80 }
-      )
-    ).toBe(dedent`
+    test(dedent`
       SELECT *
       FROM
         client
@@ -55,14 +36,7 @@ describe("select FROM", () => {
   });
 
   it(`formats long join specifications to separate lines`, () => {
-    expect(
-      pretty(
-        `SELECT * FROM client
-        LEFT JOIN client_sale ON client_sale.client_id = client.id AND client_sale.type = 287
-        RIGHT OUTER JOIN client_attribute USING (client_attribute_id, fabulously_long_col_name)`,
-        { printWidth: 80 }
-      )
-    ).toBe(dedent`
+    test(dedent`
       SELECT *
       FROM
         client
