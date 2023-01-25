56dd5ff5c9da50d9c2d2e9c407c49bd15c5f3162
Rene Saarsoo <nene@triin.net>
2023-01-25 13:03:45 +0200

    Format alter table to multiple lines

--- src/syntax/alter_table.ts
+++ src/syntax/alter_table.ts
@@ -1,11 +1,15 @@
 import { AlterTableStmt, AllAlterActionNodes } from "sql-parser-cst";
 import { CstToDocMap } from "../CstToDocMap";
+import { hardline } from "../print_utils";
 
 export const alterTableMap: Partial<
   CstToDocMap<AlterTableStmt | AllAlterActionNodes>
 > = {
-  alter_table_stmt: (print) =>
-    print.spaced(["alterTableKw", "table", "actions"]),
+  alter_table_stmt: (print) => [
+    print.spaced(["alterTableKw", "table"]),
+    hardline,
+    print("actions"),
+  ],
   alter_action_rename_table: (print) => print.spaced(["renameKw", "newName"]),
   alter_action_rename_column: (print) =>
     print.spaced(["renameKw", "oldName", "toKw", "newName"]),
--- test/alter_table.test.ts
+++ test/alter_table.test.ts
@@ -4,34 +4,41 @@ import { test } from "./test_utils";
 describe("alter table", () => {
   it(`formats ALTER TABLE..RENAME`, () => {
     test(dedent`
-      ALTER TABLE client RENAME TO org_client
+      ALTER TABLE client
+      RENAME TO org_client
     `);
   });
 
   it(`formats ALTER TABLE..RENAME COLUMN`, () => {
     test(dedent`
-      ALTER TABLE client RENAME col1 TO col2
+      ALTER TABLE client
+      RENAME col1 TO col2
     `);
     test(dedent`
-      ALTER TABLE client RENAME COLUMN col1 TO col2
+      ALTER TABLE client
+      RENAME COLUMN col1 TO col2
     `);
   });
 
   it(`formats ALTER TABLE..ADD COLUMN`, () => {
     test(dedent`
-      ALTER TABLE client ADD col1 INT
+      ALTER TABLE client
+      ADD col1 INT
     `);
     test(dedent`
-      ALTER TABLE client ADD COLUMN col1 INT
+      ALTER TABLE client
+      ADD COLUMN col1 INT
     `);
   });
 
   it(`formats ALTER TABLE..DROP COLUMN`, () => {
     test(dedent`
-      ALTER TABLE client DROP col1
+      ALTER TABLE client
+      DROP col1
     `);
     test(dedent`
-      ALTER TABLE client DROP COLUMN col1
+      ALTER TABLE client
+      DROP COLUMN col1
     `);
   });
 });
