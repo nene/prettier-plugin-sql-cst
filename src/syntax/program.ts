c04797745663c65ff91e1bb24a56b2ac1973fe1c
Rene Saarsoo <nene@triin.net>
2023-02-05 13:39:19 +0200

    Simplify statement printing logic

--- src/syntax/program.ts
+++ src/syntax/program.ts
@@ -1,21 +1,22 @@
 import { Node, Program } from "sql-parser-cst";
-import { AstPath, Doc } from "prettier";
+import { Doc } from "prettier";
 import { hardline } from "../print_utils";
 import { CstToDocMap } from "../CstToDocMap";
 
 export const programMap: CstToDocMap<Program> = {
-  program: (print, node, path) => path.map(printLineWith(print), "statements"),
+  program: (print, node) =>
+    print("statements").map((doc, i) =>
+      printStatement(doc, i, node.statements)
+    ),
 };
 
-const printLineWith =
-  <T extends Node>(print: (x: AstPath<T>) => Doc) =>
-  (childPath: AstPath<T>, i: number, all: T[]): Doc => {
-    const node = childPath.getValue();
-    if (i === 0) {
-      return print(childPath);
-    } else if (i < all.length - 1 || node.type !== "empty") {
-      return [";", hardline, hardline, print(childPath)];
-    } else {
-      return [";", print(childPath)];
-    }
-  };
+const printStatement = (doc: Doc, i: number, statements: Node[]): Doc => {
+  const node = statements[i];
+  if (i === 0) {
+    return doc;
+  } else if (i < statements.length - 1 || node.type !== "empty") {
+    return [";", hardline, hardline, doc];
+  } else {
+    return [";", doc];
+  }
+};
