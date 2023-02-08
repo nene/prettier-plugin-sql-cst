40f9fd255dd96ac72d6532cddea77a0ba2af7432
Rene Saarsoo <nene@triin.net>
2023-02-08 14:59:28 +0200

    Add failing tests for DROP SNAPSHOT/EXTERNAL TABLE

--- test/drop_table.test.ts
+++ test/drop_table.test.ts
@@ -1,16 +1,19 @@
-import dedent from "dedent-js";
 import { test } from "./test_utils";
 
 describe("drop table", () => {
   it(`formats DROP TABLE statement`, () => {
-    test(dedent`
-      DROP TABLE client
-    `);
+    test(`DROP TABLE client`);
   });
 
   it(`formats IF EXISTS`, () => {
-    test(dedent`
-      DROP TABLE IF EXISTS schm.client
-    `);
+    test(`DROP TABLE IF EXISTS schm.client`);
+  });
+
+  it.skip(`formats DROP SNAPSHOT table`, () => {
+    test(`DROP SNAPSHOT TABLE foo`, { dialect: "bigquery" });
+  });
+
+  it.skip(`formats DROP EXTERNAL table`, () => {
+    test(`DROP EXTERNAL TABLE foo`, { dialect: "bigquery" });
   });
 });
