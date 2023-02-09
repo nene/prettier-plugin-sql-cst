a8ed8fd4fad13949aa95d03eda88203b2ce4e6ae
Rene Saarsoo <nene@triin.net>
2023-02-09 12:19:20 +0200

    Perform CST transform to add final semicolon

--- src/syntax/program.ts
+++ src/syntax/program.ts
@@ -1,24 +1,22 @@
 import { Node, Program } from "sql-parser-cst";
 import { Doc } from "prettier";
-import { group, hardline, softline } from "../print_utils";
+import { hardline } from "../print_utils";
 import { CstToDocMap } from "../CstToDocMap";
 
 export const programMap: CstToDocMap<Program> = {
-  program: (print, node) => [
+  program: (print, node) =>
     print("statements").map((doc, i) =>
       printStatement(doc, i, node.statements)
     ),
-    hardline,
-  ],
 };
 
 const printStatement = (doc: Doc, i: number, statements: Node[]): Doc => {
   const node = statements[i];
   if (i === 0) {
-    return [doc, ";"];
-  } else if (node.type === "empty") {
-    return group([softline, doc]);
+    return doc;
+  } else if (node.type !== "empty") {
+    return [";", hardline, hardline, doc];
   } else {
-    return [hardline, hardline, doc, ";"];
+    return [";", hardline, doc];
   }
 };
