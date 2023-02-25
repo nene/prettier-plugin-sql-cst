b98f8a9a448fa8a16a5aa9957ea2e337e999eb69
Rene Saarsoo <nene@triin.net>
2023-02-25 16:47:01 +0200

    Generalize capacity test

---
+++ test/bigquery/bigquery.test.ts
@@ -0,0 +1,21 @@
+import dedent from "dedent-js";
+import { testBigquery } from "../test_utils";
+
+describe("bigquery", () => {
+  ["CAPACITY"].forEach((entityType) => {
+    it(`formats CREATE ${entityType}`, () => {
+      testBigquery(dedent`
+        CREATE ${entityType} commitment_id
+        OPTIONS(slot_count = 100, plan = 'FLEX')
+      `);
+    });
+
+    it(`formats DROP ${entityType}`, () => {
+      testBigquery(`DROP ${entityType} commitment_id`);
+    });
+
+    it(`formats DROP ${entityType} IF EXISTS`, () => {
+      testBigquery(`DROP ${entityType} IF EXISTS commitment_id`);
+    });
+  });
+});
