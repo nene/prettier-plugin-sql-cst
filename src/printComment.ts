ac1fbb3b376ee6a1efbb14626004a37ca6d72aaf
Rene Saarsoo <nene@triin.net>
2023-02-26 11:54:03 +0200

    Extract printComment() to separate file

---
+++ src/printComment.ts
@@ -0,0 +1,12 @@
+import { AstPath } from "prettier";
+import { Whitespace } from "sql-parser-cst";
+
+export const printComment = (path: AstPath<Whitespace>): string => {
+  const node = path.getValue();
+  if (node.type === "line_comment") {
+    if (!/^--([ \t]|$)/.test(node.text)) {
+      return node.text.replace(/^--/, "-- ");
+    }
+  }
+  return node.text;
+};
