19fcac92846d70fdce1073444805e8de9b076cc1
Rene Saarsoo <nene@triin.net>
2023-03-22 10:51:13 +0200

    Move function-tests to separate file

--- test/expr/expr.test.ts
+++ test/expr/expr.test.ts
@@ -137,54 +137,6 @@ describe("expr", () => {
     });
   });
 
-  describe("function-like", () => {
-    it(`formats function call to single line`, () => {
-      expect(pretty(`SELECT sqrt(1, 2, 3)`, { printWidth: 16 })).toBe(dedent`
-        SELECT
-          sqrt(1, 2, 3)
-      `);
-    });
-
-    it(`formats function call to multiple lines`, () => {
-      expect(pretty(`SELECT sqrt(1, 2, 3)`, { printWidth: 10 })).toBe(dedent`
-        SELECT
-          sqrt(
-            1,
-            2,
-            3
-          )
-      `);
-    });
-
-    it(`formats count(*) func call`, () => {
-      test(`SELECT count(*)`);
-    });
-
-    it(`formats count(DISTINCT) func call`, () => {
-      test(`SELECT count(DISTINCT id)`);
-    });
-
-    it(`formats CAST expression`, () => {
-      test(`SELECT CAST(127 AS INT)`);
-    });
-
-    it(`formats CAST() with FORMAT`, () => {
-      testBigquery(`SELECT CAST('11-08' AS DATE FORMAT 'DD-MM')`);
-      testBigquery(
-        `SELECT CAST('12:35' AS TIME FORMAT 'HH:MI' AT TIME ZONE 'UTC')`
-      );
-    });
-
-    it(`formats RAISE expression`, () => {
-      test(`SELECT RAISE(IGNORE), RAISE(ABORT, 'Oh no!')`);
-    });
-
-    it(`formats EXTRACT() expression`, () => {
-      testBigquery(`SELECT EXTRACT(MONTH FROM DATE '2002-08-16')`);
-      testBigquery(`SELECT EXTRACT(WEEK(SUNDAY) FROM date)`);
-    });
-  });
-
   describe("BigQuery", () => {
     it(`formats BigQuery quoted table names`, () => {
       testBigquery("SELECT * FROM `my-project.mydataset.mytable`");
---
+++ test/expr/func.test.ts
@@ -0,0 +1,57 @@
+import dedent from "dedent-js";
+import { pretty, test, testBigquery } from "../test_utils";
+
+// Functions and function-like language constructs
+describe("functions", () => {
+  it(`formats function call to single line`, () => {
+    expect(pretty(`SELECT sqrt(1, 2, 3)`, { printWidth: 16 })).toBe(dedent`
+      SELECT
+        sqrt(1, 2, 3)
+    `);
+  });
+
+  it(`formats function call to multiple lines`, () => {
+    expect(pretty(`SELECT sqrt(1, 2, 3)`, { printWidth: 10 })).toBe(dedent`
+      SELECT
+        sqrt(
+          1,
+          2,
+          3
+        )
+    `);
+  });
+
+  it(`formats count(*) func call`, () => {
+    test(`SELECT count(*)`);
+  });
+
+  it(`formats count(DISTINCT) func call`, () => {
+    test(`SELECT count(DISTINCT id)`);
+  });
+
+  describe("cast", () => {
+    it(`formats CAST expression`, () => {
+      test(`SELECT CAST(127 AS INT)`);
+    });
+
+    it(`formats CAST() with FORMAT`, () => {
+      testBigquery(`SELECT CAST('11-08' AS DATE FORMAT 'DD-MM')`);
+      testBigquery(
+        `SELECT CAST('12:35' AS TIME FORMAT 'HH:MI' AT TIME ZONE 'UTC')`
+      );
+    });
+  });
+
+  describe("raise", () => {
+    it(`formats RAISE expression`, () => {
+      test(`SELECT RAISE(IGNORE), RAISE(ABORT, 'Oh no!')`);
+    });
+  });
+
+  describe("extract", () => {
+    it(`formats EXTRACT() expression`, () => {
+      testBigquery(`SELECT EXTRACT(MONTH FROM DATE '2002-08-16')`);
+      testBigquery(`SELECT EXTRACT(WEEK(SUNDAY) FROM date)`);
+    });
+  });
+});
