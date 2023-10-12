773370521eced525094b8432cc8750ec51438ae8
Rene Saarsoo <nene@triin.net>
2023-10-12 11:23:15 +0300

    Temporarily disable embedding

--- src/embed.ts
+++ src/embed.ts
@@ -1,8 +1,8 @@
-import { Printer } from "prettier";
-import { Node } from "sql-parser-cst";
-import { embedJs } from "./embedJs";
-import { embedJson } from "./embedJson";
+// import { Printer } from "prettier";
+// import { Node } from "sql-parser-cst";
+// import { embedJs } from "./embedJs";
+// import { embedJson } from "./embedJson";
 
-export const embed: NonNullable<Printer<Node>["embed"]> = (...args) => {
-  return embedJson(...args) || embedJs(...args);
-};
+// export const embed: NonNullable<Printer<Node>["embed"]> = (...args) => {
+//   return embedJson(...args) || embedJs(...args);
+// };
