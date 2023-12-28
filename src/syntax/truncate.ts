998691df37303d020431b9d72f0ae7ad27a0941b
Rene Saarsoo <nene@triin.net>
2023-12-28 19:45:47 +0200

    Fix formatting of truncate_stmt

--- src/syntax/truncate.ts
+++ src/syntax/truncate.ts
@@ -2,5 +2,5 @@ import { TruncateStmt } from "sql-parser-cst";
 import { CstToDocMap } from "../CstToDocMap";
 
 export const truncateMap: CstToDocMap<TruncateStmt> = {
-  truncate_stmt: (print) => print.spaced(["truncateTableKw", "table"]),
+  truncate_stmt: (print) => print.spaced(["truncateKw", "tableKw", "table"]),
 };
