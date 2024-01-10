084e13a32aaa27ce8aca11883faf2c94cd0e03c3
Rene Saarsoo <nene@triin.net>
2024-01-10 15:57:12 +0200

    Format multi-table TRUNCATE

--- src/syntax/truncate.ts
+++ src/syntax/truncate.ts
@@ -1,13 +1,25 @@
 import { TruncateStmt } from "sql-parser-cst";
 import { CstToDocMap } from "../CstToDocMap";
+import { group, indent, join, line } from "../print_utils";
+import { isDefined } from "../utils";
 
 export const truncateMap: CstToDocMap<TruncateStmt> = {
-  truncate_stmt: (print) =>
-    print.spaced([
-      "truncateKw",
-      "tableKw",
-      "tables",
-      "cascadeOrRestrictKw",
-      "restartOrContinueKw",
-    ]),
+  truncate_stmt: (print, node) => [
+    print.spaced(["truncateKw", "tableKw"]),
+    group(
+      indent([
+        line,
+        join(
+          line,
+          [
+            print("tables"),
+            node.restartOrContinueKw
+              ? print.spaced("restartOrContinueKw")
+              : undefined,
+            node.cascadeOrRestrictKw ? print("cascadeOrRestrictKw") : undefined,
+          ].filter(isDefined),
+        ),
+      ]),
+    ),
+  ],
 };
--- test/dml/truncate.test.ts
+++ test/dml/truncate.test.ts
@@ -1,3 +1,4 @@
+import dedent from "dedent-js";
 import { testBigquery, testPostgresql } from "../test_utils";
 
 describe("truncate", () => {
@@ -11,5 +12,17 @@ describe("truncate", () => {
 
   it(`formats {RESTART | CONTINUE} IDENTITY`, async () => {
     await testPostgresql(`TRUNCATE TABLE dataset.employee RESTART IDENTITY`);
+  });
+
+  it(`formats multi-table truncate with modifiers`, async () => {
+    await testPostgresql(dedent`
+      TRUNCATE TABLE
+        dataset.employee,
+        dataset.manager,
+        dataset.department,
+        dataset.company
+        CONTINUE IDENTITY
+        RESTRICT
+    `);
   });
 });
