d6098e82204039777adc7c471408bd3701318893
Rene Saarsoo <nene@triin.net>
2024-02-03 13:46:14 +0200

    Reduce number of lines in schema tests

--- test/ddl/schema.test.ts
+++ test/ddl/schema.test.ts
@@ -4,83 +4,65 @@ import { testBigquery } from "../test_utils";
 describe("schema", () => {
   describe("create schema", () => {
     it(`formats CREATE SCHEMA`, async () => {
-      await testBigquery(
-        dedent`
-          CREATE SCHEMA schema_name
-        `,
-      );
+      await testBigquery(dedent`
+        CREATE SCHEMA schema_name
+      `);
     });
 
     it(`formats IF NOT EXISTS`, async () => {
-      await testBigquery(
-        dedent`
-          CREATE SCHEMA IF NOT EXISTS schema_name
-        `,
-      );
+      await testBigquery(dedent`
+        CREATE SCHEMA IF NOT EXISTS schema_name
+      `);
     });
 
     it(`formats OPTIONS (..)`, async () => {
-      await testBigquery(
-        dedent`
-          CREATE SCHEMA schema_name
-          OPTIONS (friendly_name = 'Happy schema')
-        `,
-      );
+      await testBigquery(dedent`
+        CREATE SCHEMA schema_name
+        OPTIONS (friendly_name = 'Happy schema')
+      `);
     });
 
     it(`formats DEFAULT COLLATE`, async () => {
-      await testBigquery(
-        dedent`
-          CREATE SCHEMA schema_name
-          DEFAULT COLLATE 'und:ci'
-        `,
-      );
+      await testBigquery(dedent`
+        CREATE SCHEMA schema_name
+        DEFAULT COLLATE 'und:ci'
+      `);
     });
   });
 
   describe("drop schema", () => {
     it(`formats DROP SCHEMA`, async () => {
-      await testBigquery(
-        dedent`
-          DROP SCHEMA schema_name
-        `,
-      );
+      await testBigquery(dedent`
+        DROP SCHEMA schema_name
+      `);
     });
 
     it(`formats IF EXISTS`, async () => {
-      await testBigquery(
-        dedent`
-          DROP SCHEMA IF EXISTS schema_name
-        `,
-      );
+      await testBigquery(dedent`
+        DROP SCHEMA IF EXISTS schema_name
+      `);
     });
 
     it(`formats CASCADE/RESTRICT`, async () => {
-      await testBigquery(
-        dedent`
-          DROP SCHEMA schema_name CASCADE
-        `,
-      );
+      await testBigquery(dedent`
+        DROP SCHEMA schema_name CASCADE
+      `);
     });
   });
 
   describe("alter schema", () => {
     it(`formats ALTER SCHEMA .. SET OPTIONS`, async () => {
-      await testBigquery(
-        dedent`
-          ALTER SCHEMA IF EXISTS my_schema
-          SET OPTIONS (description = 'blah')
-        `,
-      );
+      await testBigquery(dedent`
+        ALTER SCHEMA IF EXISTS my_schema
+        SET OPTIONS (description = 'blah')
+      `);
     });
 
     it(`formats ALTER SCHEMA .. SET DEFAULT COLLATE`, async () => {
-      await testBigquery(
-        dedent`
-          ALTER SCHEMA my_schema
-          SET DEFAULT COLLATE 'und:ci'
-        `,
-      );
+      await testBigquery(dedent`
+        ALTER SCHEMA my_schema
+        SET DEFAULT COLLATE 'und:ci'
+      `);
     });
   });
 });
