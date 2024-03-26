7897bba0c2b8c03f3bc68693e33c0818f133eb42
Rene Saarsoo <nene@triin.net>
2024-03-26 17:23:17 +0200

    Format PostgreSQL CREATE TRIGGER syntax

--- src/syntax/trigger.ts
+++ src/syntax/trigger.ts
@@ -2,10 +2,17 @@ import { AllTriggerNodes } from "sql-parser-cst";
 import { group, hardline, indent, join, line } from "../print_utils";
 import { CstToDocMap } from "../CstToDocMap";
 
-export const triggerMap: Partial<CstToDocMap<AllTriggerNodes>> = {
+export const triggerMap: CstToDocMap<AllTriggerNodes> = {
   create_trigger_stmt: (print, node) =>
     join(hardline, [
-      print.spaced(["createKw", "kind", "triggerKw", "ifNotExistsKw", "name"]),
+      print.spaced([
+        "createKw",
+        "orReplaceKw",
+        "kind",
+        "triggerKw",
+        "ifNotExistsKw",
+        "name",
+      ]),
       group([
         node.timeKw ? [print.spaced("timeKw"), " "] : [],
         print("event"),
@@ -24,6 +31,21 @@ export const triggerMap: Partial<CstToDocMap<AllTriggerNodes>> = {
   when_clause: (print) =>
     group([print("whenKw"), indent([line, print("expr")])]),
   for_each_clause: (print) => print.spaced(["forEachKw", "itemKw"]),
+  execute_clause: (print) => [
+    print.spaced(["executeKw", "functionKw", "name"]),
+    print("args"),
+  ],
+  from_referenced_table_clause: (print) => print.spaced(["fromKw", "table"]),
+  trigger_timing_clause: (print) => print.spaced(["timingKw"]),
+  trigger_referencing_clause: (print) =>
+    group([
+      print.spaced(["referencingKw"]),
+      indent([line, print("transitions")]),
+    ]),
+  trigger_transition: (print) => [
+    print.spaced(["oldOrNewKw", "rowOrTableKw", "asKw", "name"]),
+  ],
+
   drop_trigger_stmt: (print) =>
     print.spaced(["dropTriggerKw", "ifExistsKw", "trigger"]),
 };
