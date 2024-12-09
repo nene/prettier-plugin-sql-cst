fff83ed59d4036da55f14ece990eb3866910499d
Rene Saarsoo <nene@triin.net>
2024-12-09 10:23:41 +0200

    Create separate group for GRANT/REVOKE tests for BigQuery

--- test/dcl/grant.test.ts
+++ test/dcl/grant.test.ts
@@ -2,47 +2,49 @@ import dedent from "dedent-js";
 import { testBigquery } from "../test_utils";
 
 describe("grant", () => {
-  it(`formats short GRANT in single line`, async () => {
-    await testBigquery(dedent`
-      GRANT \`roles/x\` ON TABLE revenue TO 'user:tom'
-    `);
-  });
+  describe("bigquery", () => {
+    it(`formats short GRANT in single line`, async () => {
+      await testBigquery(dedent`
+        GRANT \`roles/x\` ON TABLE revenue TO 'user:tom'
+      `);
+    });
 
-  it(`formats short GRANT in multiple lines if user prefers`, async () => {
-    await testBigquery(dedent`
-      GRANT \`roles/x\`
-      ON TABLE revenue
-      TO 'user:tom'
-    `);
-  });
+    it(`formats short GRANT in multiple lines if user prefers`, async () => {
+      await testBigquery(dedent`
+        GRANT \`roles/x\`
+        ON TABLE revenue
+        TO 'user:tom'
+      `);
+    });
 
-  it(`formats GRANT (single role, single user)`, async () => {
-    await testBigquery(dedent`
-      GRANT \`roles/bigquery.dataViewer\`
-      ON TABLE myCompany.revenue
-      TO 'user:tom@example.com'
-    `);
-  });
+    it(`formats GRANT (single privilege, single user)`, async () => {
+      await testBigquery(dedent`
+        GRANT \`roles/bigquery.dataViewer\`
+        ON TABLE myCompany.revenue
+        TO 'user:tom@example.com'
+      `);
+    });
 
-  it(`formats GRANT (multiple roles, multiple users)`, async () => {
-    await testBigquery(dedent`
-      GRANT \`roles/bigquery.dataViewer\`, \`roles/bigquery.admin\`
-      ON SCHEMA myCompany
-      TO 'user:tom@example.com', 'user:sara@example.com'
-    `);
-  });
+    it(`formats GRANT (multiple privileges, multiple users)`, async () => {
+      await testBigquery(dedent`
+        GRANT \`roles/bigquery.dataViewer\`, \`roles/bigquery.admin\`
+        ON SCHEMA myCompany
+        TO 'user:tom@example.com', 'user:sara@example.com'
+      `);
+    });
 
-  it(`formats GRANT (multiline list of roles and users)`, async () => {
-    await testBigquery(dedent`
-      GRANT
-        \`roles/bigquery.dataViewer\`,
-        \`roles/bigquery.admin\`,
-        \`roles/bigquery.rowAccessPolicies.create\`
-      ON SCHEMA myCompany
-      TO
-        'user:tom@example.com',
-        'user:sara@example.com',
-        'specialGroup:allAuthenticatedUsers'
-    `);
+    it(`formats GRANT (multiline list of privileges and users)`, async () => {
+      await testBigquery(dedent`
+        GRANT
+          \`roles/bigquery.dataViewer\`,
+          \`roles/bigquery.admin\`,
+          \`roles/bigquery.rowAccessPolicies.create\`
+        ON SCHEMA myCompany
+        TO
+          'user:tom@example.com',
+          'user:sara@example.com',
+          'specialGroup:allAuthenticatedUsers'
+      `);
+    });
   });
 });
--- test/dcl/revoke.test.ts
+++ test/dcl/revoke.test.ts
@@ -2,47 +2,49 @@ import dedent from "dedent-js";
 import { testBigquery } from "../test_utils";
 
 describe("revoke", () => {
-  it(`formats short REVOKE in single line`, async () => {
-    await testBigquery(dedent`
-      REVOKE \`roles/x\` ON VIEW revenue FROM 'user:tom'
-    `);
-  });
+  describe("bigquery", () => {
+    it(`formats short REVOKE in single line`, async () => {
+      await testBigquery(dedent`
+        REVOKE \`roles/x\` ON VIEW revenue FROM 'user:tom'
+      `);
+    });
 
-  it(`formats short REVOKE in multiple lines if user prefers`, async () => {
-    await testBigquery(dedent`
-      REVOKE \`roles/x\`
-      ON VIEW revenue
-      FROM 'user:tom'
-    `);
-  });
+    it(`formats short REVOKE in multiple lines if user prefers`, async () => {
+      await testBigquery(dedent`
+        REVOKE \`roles/x\`
+        ON VIEW revenue
+        FROM 'user:tom'
+      `);
+    });
 
-  it(`formats REVOKE (single role, single user)`, async () => {
-    await testBigquery(dedent`
-      REVOKE \`roles/bigquery.dataViewer\`
-      ON VIEW myCompany.revenue
-      FROM 'user:tom@example.com'
-    `);
-  });
+    it(`formats REVOKE (single privilege, single user)`, async () => {
+      await testBigquery(dedent`
+        REVOKE \`roles/bigquery.dataViewer\`
+        ON VIEW myCompany.revenue
+        FROM 'user:tom@example.com'
+      `);
+    });
 
-  it(`formats REVOKE (multiple roles, multiple users)`, async () => {
-    await testBigquery(dedent`
-      REVOKE \`roles/bigquery.dataViewer\`, \`roles/bigquery.admin\`
-      ON SCHEMA myCompany
-      FROM 'user:tom@example.com', 'user:sara@example.com'
-    `);
-  });
+    it(`formats REVOKE (multiple privileges, multiple users)`, async () => {
+      await testBigquery(dedent`
+        REVOKE \`roles/bigquery.dataViewer\`, \`roles/bigquery.admin\`
+        ON SCHEMA myCompany
+        FROM 'user:tom@example.com', 'user:sara@example.com'
+      `);
+    });
 
-  it(`formats REVOKE (multiline list of roles and users)`, async () => {
-    await testBigquery(dedent`
-      REVOKE
-        \`roles/bigquery.dataViewer\`,
-        \`roles/bigquery.admin\`,
-        \`roles/bigquery.rowAccessPolicies.create\`
-      ON EXTERNAL TABLE myCompany
-      FROM
-        'user:tom@example.com',
-        'user:sara@example.com',
-        'specialGroup:allAuthenticatedUsers'
-    `);
+    it(`formats REVOKE (multiline list of privileges and users)`, async () => {
+      await testBigquery(dedent`
+        REVOKE
+          \`roles/bigquery.dataViewer\`,
+          \`roles/bigquery.admin\`,
+          \`roles/bigquery.rowAccessPolicies.create\`
+        ON EXTERNAL TABLE myCompany
+        FROM
+          'user:tom@example.com',
+          'user:sara@example.com',
+          'specialGroup:allAuthenticatedUsers'
+      `);
+    });
   });
 });
