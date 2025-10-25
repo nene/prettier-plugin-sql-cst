cba381fce08df2af1c66609eaeb18750bcff950b
Rene Saarsoo <nene@triin.net>
2025-10-25 23:00:45 +0300

    Remove Partial<> wrapper from prepared statements

--- src/syntax/prepared_statements.ts
+++ src/syntax/prepared_statements.ts
@@ -2,9 +2,7 @@ import { AllPreparedStatementNodes } from "sql-parser-cst";
 import { group, hardline, indent, join, line } from "../print_utils";
 import { CstToDocMap } from "../CstToDocMap";
 
-export const preparedStatementsMap: Partial<
-  CstToDocMap<AllPreparedStatementNodes>
-> = {
+export const preparedStatementsMap: CstToDocMap<AllPreparedStatementNodes> = {
   // EXECUTE
   execute_stmt: (print, node) => {
     if (node.args?.type === "execute_using_clause") {
