452580f3b505c6533f32e179e3cb189899b9cec4
Rene Saarsoo <nene@triin.net>
2025-10-26 21:12:24 +0200

    Format PostgreSQL SET, RESET & SHOW statements

--- src/syntax/parameter.ts
+++ src/syntax/parameter.ts
@@ -1,11 +1,23 @@
 import { AllParameterNodes } from "sql-parser-cst";
 import { CstToDocMap } from "../CstToDocMap";
 
-export const parameterMap: Partial<CstToDocMap<AllParameterNodes>> = {
+export const parameterMap: CstToDocMap<AllParameterNodes> = {
+  // SET
+  set_parameter_stmt: (print) =>
+    print.spaced(["setKw", "modifierKw", "name", "operator", "value"]),
+  set_time_zone_parameter_stmt: (print) =>
+    print.spaced(["setKw", "modifierKw", "timeZoneKw", "value"]),
   set_parameter_clause: (print) =>
     print.spaced(["setKw", "name", "operator", "value"]),
+  local_parameter_value: (print) => print("localKw"),
   set_parameter_from_current_clause: (print) =>
     print.spaced(["setKw", "name", "fromCurrentKw"]),
+
+  // RESET
+  reset_parameter_stmt: (print) => print.spaced(["resetKw", "name"]),
   reset_parameter_clause: (print) => print.spaced(["resetKw", "name"]),
   all_parameters: (print) => print("allKw"),
+
+  // SHOW
+  show_parameter_stmt: (print) => print.spaced(["showKw", "name"]),
 };
---
+++ test/postgresql/parameter.test.ts
@@ -0,0 +1,52 @@
+import dedent from "dedent-js";
+import { testPostgresql } from "../test_utils";
+
+describe("parameters", () => {
+  it(`formats SET statement`, async () => {
+    await testPostgresql(dedent`
+      SET work_mem TO '64MB'
+    `);
+  });
+
+  it(`formats SET [LOCAL] statement`, async () => {
+    await testPostgresql(dedent`
+      SET LOCAL max_connections = 200
+    `);
+  });
+
+  it(`formats SET TIME ZONE statement`, async () => {
+    await testPostgresql(dedent`
+      SET TIME ZONE 'UTC'
+    `);
+  });
+
+  it(`formats SET [SESSION] TIME ZONE LOCAL`, async () => {
+    await testPostgresql(dedent`
+      SET SESSION TIME ZONE LOCAL
+    `);
+  });
+
+  it(`formats RESET statement`, async () => {
+    await testPostgresql(dedent`
+      RESET work_mem
+    `);
+  });
+
+  it(`formats RESET ALL`, async () => {
+    await testPostgresql(dedent`
+      RESET ALL
+    `);
+  });
+
+  it(`formats SHOW statement`, async () => {
+    await testPostgresql(dedent`
+      SHOW work_mem
+    `);
+  });
+
+  it(`formats SHOW ALL`, async () => {
+    await testPostgresql(dedent`
+      SHOW ALL
+    `);
+  });
+});
