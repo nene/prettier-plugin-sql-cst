d4edfdfb373b97fe52eeefcfeef7ad7a10998dfb
Rene Saarsoo <nene@triin.net>
2025-11-21 20:50:42 +0200

    Improve formatting of ANALYZE

--- src/syntax/analyze.ts
+++ src/syntax/analyze.ts
@@ -1,6 +1,8 @@
 import { AnalyzeStmt } from "sql-parser-cst";
 import { CstToDocMap } from "../CstToDocMap";
+import { group } from "../print_utils";
 
 export const analyzeMap: CstToDocMap<AnalyzeStmt> = {
-  analyze_stmt: (print) => print.spaced(["analyzeKw", "tableKw", "tables"]),
+  analyze_stmt: (print) =>
+    group(print.spaced(["analyzeKw", "tableKw", "tables"])),
 };
--- test/analyze.test.ts
+++ test/analyze.test.ts
@@ -5,6 +5,14 @@ describe("analyze", () => {
     await test(`ANALYZE my_schema.my_table`);
   });
 
+  it(`formats ANALYZE TABLE statement`, async () => {
+    await test(`ANALYZE TABLE foo`);
+  });
+
+  it(`formats multiple tables`, async () => {
+    await test(`ANALYZE foo, bar, baz`);
+  });
+
   it(`formats plain ANALYZE statement`, async () => {
     await test(`ANALYZE`);
   });
