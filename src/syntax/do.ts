f62ef09acda3dfe3f1a843ff48684061dedc435c
Rene Saarsoo <nene@triin.net>
2025-11-21 20:53:07 +0200

    Wrap DO statement in group() just in case

--- src/syntax/do.ts
+++ src/syntax/do.ts
@@ -1,6 +1,7 @@
 import { DoStmt } from "sql-parser-cst";
 import { CstToDocMap } from "../CstToDocMap";
+import { group } from "../print_utils";
 
 export const doMap: CstToDocMap<DoStmt> = {
-  do_stmt: (print) => print.spaced(["doKw", "language", "body"]),
+  do_stmt: (print) => group(print.spaced(["doKw", "language", "body"])),
 };
