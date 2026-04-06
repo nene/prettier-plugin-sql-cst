ce831c2fa7c9c3e4a72ac6cae53216dc2fcce4d7
Rene Saarsoo <nene@triin.net>
2026-04-06 21:12:19 +0300

    Fix analyze-field-access script

--- src/CstToDocMap.ts
+++ src/CstToDocMap.ts
@@ -1,12 +1,14 @@
 import { Node } from "sql-parser-cst";
 import { AstPath, Doc } from "prettier";
-import { PrintFn } from "./PrintFn";
+import { PrintableKey, PrintFn } from "./PrintFn";
 import { AllPrettierOptions } from "./options";
 
 export type NodeByType<TType, TNode> = Extract<TNode, { type: TType }>;
 
 export type ToDocFn<TNode> = (
-  print: PrintFn<TNode>,
+  // Note: the second type argument could technically be inferred by TypeScript,
+  // but the analyze-field-access script relies on it being explicitly present.
+  print: PrintFn<TNode, PrintableKey<TNode>>,
   node: TNode,
   path: AstPath<TNode>,
   options: AllPrettierOptions<TNode>,
