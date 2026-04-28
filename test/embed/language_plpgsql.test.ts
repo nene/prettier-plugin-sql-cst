7963bee3fca86c333d2934969f9a57619d6d9779
Rene Saarsoo <nene@triin.net>
2026-04-28 20:18:38 +0300

    Generalize LANGUAGE plpgsql tests for FUNCTION/PROCEDURE

--- test/embed/language_plpgsql.test.ts
+++ test/embed/language_plpgsql.test.ts
@@ -1,52 +1,55 @@
 import dedent from "dedent-js";
 import { pretty } from "../test_utils";
 
-describe("LANGUAGE plpgsql", () => {
-  describe("CREATE FUNCTION", () => {
-    it(`reformats SQL in dollar-quoted PL/pgSQL function`, async () => {
-      expect(
-        await pretty(
-          dedent`
-            CREATE FUNCTION my_func()
-            RETURNS INT
-            LANGUAGE plpgsql
-            AS $$BEGIN RETURN 1; END$$
-          `,
-          { dialect: "postgresql", sqlExperimentalPlpgsql: true },
-        ),
-      ).toBe(dedent`
-        CREATE FUNCTION my_func()
-        RETURNS INT
-        LANGUAGE plpgsql
-        AS $$
-        BEGIN
-          RETURN 1;
-        END;
-        $$
-      `);
-    });
+const statements = [
+  { name: "CREATE FUNCTION", code: "CREATE FUNCTION my_func()\nRETURNS INT" },
+  { name: "CREATE PROCEDURE", code: "CREATE PROCEDURE my_proc()" },
+];
 
-    it(`ignores plpgsql language name case`, async () => {
-      expect(
-        await pretty(
-          dedent`
-            CREATE FUNCTION my_func()
-            RETURNS INT
-            LANGUAGE "plPgSQL"
-            AS $$BEGIN RETURN 1; END$$
-          `,
-          { dialect: "postgresql", sqlExperimentalPlpgsql: true },
-        ),
-      ).toBe(dedent`
-        CREATE FUNCTION my_func()
-        RETURNS INT
-        LANGUAGE "plPgSQL"
-        AS $$
-        BEGIN
-          RETURN 1;
-        END;
-        $$
-      `);
+describe("LANGUAGE plpgsql", () => {
+  statements.forEach(({ name, code }) => {
+    describe(name, () => {
+      it(`reformats SQL in dollar-quoted PL/pgSQL function`, async () => {
+        expect(
+          await pretty(
+            dedent`
+              ${code}
+              LANGUAGE plpgsql
+              AS $$BEGIN RETURN 1; END$$
+            `,
+            { dialect: "postgresql", sqlExperimentalPlpgsql: true },
+          ),
+        ).toBe(dedent`
+          ${code}
+          LANGUAGE plpgsql
+          AS $$
+          BEGIN
+            RETURN 1;
+          END;
+          $$
+        `);
+      });
+
+      it(`ignores plpgsql language name case`, async () => {
+        expect(
+          await pretty(
+            dedent`
+              ${code}
+              LANGUAGE "plPgSQL"
+              AS $$BEGIN RETURN 1; END$$
+            `,
+            { dialect: "postgresql", sqlExperimentalPlpgsql: true },
+          ),
+        ).toBe(dedent`
+          ${code}
+          LANGUAGE "plPgSQL"
+          AS $$
+          BEGIN
+            RETURN 1;
+          END;
+          $$
+        `);
+      });
     });
   });
 });
