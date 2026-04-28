8adfa90b953cdc007155d5d1c599075256271c84
Rene Saarsoo <nene@triin.net>
2026-04-28 19:28:28 +0300

    Move LANGUAGE PYTHON tests to separate file

--- test/ddl/procedure.test.ts
+++ test/ddl/procedure.test.ts
@@ -63,36 +63,6 @@ describe("procedure", () => {
       );
     });
 
-    describe("LANGUAGE PYTHON", () => {
-      it(`formats remote python procedure`, async () => {
-        await testBigquery(
-          dedent`
-            CREATE PROCEDURE my_bq_project.my_dataset.spark_proc()
-            WITH CONNECTION \`my-project-id.us.my-connection\`
-            OPTIONS (engine = "SPARK", main_file_uri = "gs://my-bucket/my-pyspark-main.py")
-            LANGUAGE PYTHON
-          `,
-        );
-      });
-
-      it(`formats inline python procedure`, async () => {
-        await testBigquery(
-          dedent`
-            CREATE PROCEDURE spark_proc()
-            WITH CONNECTION my_connection
-            OPTIONS (engine = "SPARK")
-            LANGUAGE PYTHON
-            AS r'''
-              from pyspark.sql import SparkSession
-              spark = SparkSession.builder.appName("spark-bigquery-demo").getOrCreate()
-              # Load data from BigQuery.
-              words = spark.read.format("bigquery")
-            '''
-          `,
-        );
-      });
-    });
-
     describe("LANGUAGE sql", () => {
       it(`reformats SQL in dollar-quoted SQL procedure`, async () => {
         expect(
